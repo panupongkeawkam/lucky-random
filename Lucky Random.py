da14cd4f9fefd6e541149dd6e01ea2aa0eabdbac
unknown <63070157@kmitl.ac.th>
2020-12-04 15:03:25 +0700

    update function

--- Lucky Random.py
+++ Lucky Random.py
@@ -5,45 +5,50 @@ import string
  
 def printRandomInt():
     r_int = random.randint(1,1000000)
-    messagebox.showwarning(title="Congrat!!!", message="Have a beautiful day! %06d" %(r_int))
+    messagebox.showinfo(title="Good luck", message="Have a beautiful day! %06d" %(r_int))
  
 def printRandomFloat():
     r_float = random.randint(1,1000)
-    messagebox.showwarning(title="Congrat!!!", message="Have a good day! %03d " %(r_float))
+    messagebox.showinfo(title="Good luck", message="Have a good day! %03d " %(r_float))
  
 def printRandomSmall():
     r_small = random.randint(1,100)
-    messagebox.showwarning(title="Congrat!!!",message="Good Luck Bro! %02d" %(r_small))
+    messagebox.showinfo(title="Good luck",message="Good Luck Bro! %02d" %(r_small))
  
 def printRandomCap():
-    r_float = random.choice(["หัว", "ก้อย"])
-    messagebox.showinfo(title="Let try",message="Toss a coin heads/tails: "+r_float)
-#    messagebox.showinfo("testss"+r_float * come)
+    r_string = random.choice(["Heads", "Tails"])
+    messagebox.showinfo(title="Good luck",message="Toss a coin : "+r_string)
+
+def printRandomPassword():
+    s="abcdefghijklmnopqrstuvwxyz012345678970ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&"
+    x = random.randint(7, 13)
+    p =  "".join(random.sample(s,x))
+    messagebox.showinfo(title='Your password', message='Good luck!: %s' %p)
  
  
 class Window(Frame):
  
     def __init__(self, master=None):
-        Frame.__init__(self, master)                
+        Frame.__init__(self, master)
         self.master = master
         self.init_window()
  
     def init_window(self):
         #self.pack(fill=BOTH, expand=1)
         self.pack()
-        button1 = Button(self, text = "ลอตเตอรี่ รางวัลที่ 1!", fg = "Black", bg = "White", command = printRandomInt)
-        button2 = Button(self, text = "ลอตเตอรี่ เลขท้าย3ตัว", fg = "Black", bg = "white", command = printRandomFloat)
-        button3 = Button(self, text = "ลอตเตอรี่ เลขท้าย2ตัว", fg = "Black", bg = "white", command = printRandomSmall)
-        button4 = Button(self, text = "โยนเหรียญ หัว&ก้อย", fg = "Black", bg = "White", command = printRandomCap)
-        button1.grid(row = 1, column = 1,padx=125,pady=50)
-        button2.grid(row = 1, column = 2,padx=125,pady=50)
-        button3.grid(row = 2, column = 1,padx=125,pady=50)
-        button4.grid(row = 2, column = 2,padx=125,pady=50)
+        button1 = Button(self, text = "Lottery 1st prize", fg = "Black", bg = "White", command = printRandomInt)
+        button2 = Button(self, text = "Lottery the three digit prize ", fg = "Black", bg = "white", command = printRandomFloat)
+        button3 = Button(self, text = "Random Password", fg = "Black", bg = "white", command = printRandomPassword)
+        button4 = Button(self, text = "Toss a coin heads/tails", fg = "Black", bg = "White", command = printRandomCap)
+        button1.grid(row = 1, column = 1,padx=30,pady=50)
+        button2.grid(row = 1, column = 2,padx=30,pady=50)
+        button3.grid(row = 2, column = 1,padx=30,pady=30)
+        button4.grid(row = 2, column = 2,padx=30,pady=30)
  
 root = Tk()
-root.configure(background = "light blue")
+root.configure(background = "white")
 root.title("Random Generate")
-root.geometry("1000x1000")
+root.geometry("400x250")
 
 app = Window(root)
 root.mainloop()
