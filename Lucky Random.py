2edf801ff18b4a4fe226c8708c91930afdbf128f
pithawat565 <71390474+pithawat565@users.noreply.github.com>
2020-12-07 14:59:58 +0700

    Update Lucky Random.py

--- Lucky Random.py
+++ Lucky Random.py
@@ -1,3 +1,4 @@
+  
 from tkinter import *
 from tkinter import messagebox
 import random, string, webbrowser
@@ -8,7 +9,7 @@ def Random2Digit():
     r_2digit = random.randint(1, 100) # สุ่มเลข 01 ถึง 99
     messagebox.showinfo(title="Good luck", message="Good Luck Bro! %02d" %(r_2digit))
 
-def Randon3Digit():
+def Random3Digit():
     """3 digit number random function"""
     r_3digit = random.randint(1, 1000) # สุ่มเลข 001 ถึง 999
     messagebox.showinfo(title="Good luck", message="Have a good day! %03d " %(r_3digit))
@@ -16,7 +17,9 @@ def Randon3Digit():
 def Random6Digit():
     """6 digit number random function"""
     r_6digit = random.randint(1, 1000000) # สุ่มเลข 000001 ถึง 999999
-    messagebox.showinfo(title="Good luck", message="Have a beautiful day! %06d" %(r_6digit))
+    condition = messagebox.askretrycancel(title="Good luck", message="Have a beautiful day! %06d" %(r_6digit))
+    if condition == True: 
+        Random6Digit() # กด retry แล้วใส่เพื่อสุ่มเลขใหม่
 
 def RandomTossCoin():
     """Toss coin heads or tails function"""
@@ -53,9 +56,9 @@ def RandomMusicPlaylist():
        "https://www.youtube.com/watch?v=Nj2U6rhnucI&list=PL4o29bINVT4EG_y-k5jGoOu3-Am8Nvi10&index=11",\
        "https://www.youtube.com/watch?v=dqRZDebPIGs&list=PL4o29bINVT4EG_y-k5jGoOu3-Am8Nvi10&index=12",\
         "https://www.youtube.com/watch?v=VF-r5TtlT9w&list=PL4o29bINVT4EG_y-k5jGoOu3-Am8Nvi10&index=15",\
-        "https://www.youtube.com/watch?v=9HDEHj2yzew&list=PL4o29bINVT4EG_y-k5jGoOu3-Am8Nvi10&index=17"] # ลิงค์ของรายการเพลง (YouTube)
-    webbrowser.open_new_tab(random.choice(url)) # สุ่มรายการเพลงใน browser ของผู้ใช้โดยเปิด Tab ใหม่
-
+        "https://www.youtube.com/watch?v=9HDEHj2yzew&list=PL4o29bINVT4EG_y-k5jGoOu3-Am8Nvi10&index=17",\
+        "https://www.youtube.com/watch?v=1gSe78TIEEk&list=RD1gSe78TIEEk&start_radio=1"] # ลิงค์ของรายการเพลง (YouTube)
+    webbrowser.open_new_tab(random.choice(url)) # สุ่มรายการเพลงใน browser ของผู้ใช้โดยเปิด Tab 
 class Window(Frame):
 
     def __init__(self, master=None):
@@ -65,21 +68,28 @@ class Window(Frame):
 
     def init_window(self):
         # self.pack(fill=BOTH, expand=1)
-        self.pack()
-        button1 = Button(self, text="Lottery 1st prize", fg="Black", bg="White", command=Random6Digit) # ปุ่มสุ่มเลข6หลัก
-        button2 = Button(self, text="Random Menu ", fg="Black", bg="white", command=RandomMenu) # ปุ่มสุ่มอาหาร
-        button3 = Button(self, text="Random Password", fg="Black", bg="white", command=RandomPassword) # ปุ่มสุ่ม Password
-        button4 = Button(self, text="Listen music playlist", fg="Black", bg="White", command=RandomMusicPlaylist) # ปุ่มสุ่มเพลง
+        self.pack(fill=BOTH, side=LEFT, expand=True)
+        button1 = Button(self, text="Lottery 1st prize", font="72", fg="white", bg="red", borderwidth="5", command=Random6Digit) # ปุ่มสุ่มเลข6หลัก
+        button2 = Button(self, text="Random Menu ", font="72", fg="white", bg="green", borderwidth="5", command=RandomMenu) # ปุ่มสุ่มอาหาร
+        button3 = Button(self, text="Random Password", font="72", fg="white", bg="blue", borderwidth="5", command=RandomPassword) # ปุ่มสุ่ม Password
+        button4 = Button(self, text="Listen music playlist", font="72", fg="white", bg="light blue", borderwidth="5", command=RandomMusicPlaylist) # ปุ่มสุ่มเพลง
+        button5 = Button(self, text="Random 2 Digits", font="72", fg="white", bg="light blue", borderwidth="5", command=Random2Digit)
+        button6 = Button(self, text="Random 3 Digits", font="72", fg="white", bg="green", borderwidth="5", command=Random3Digit)
         # ตำแหน่งของปุ่ม
-        button1.grid(row=1, column=1, padx=10, pady=50)
-        button2.grid(row=1, column=2, padx=10, pady=50)
-        button3.grid(row=2, column=1, padx=10, pady=30)
-        button4.grid(row=2, column=2, padx=10, pady=30)
-
+        button1.pack(fill=BOTH, side=TOP, expand=True)
+        button2.pack(fill=BOTH, side=TOP, expand=True)
+        button3.pack(fill=BOTH, side=TOP, expand=True)
+        button4.pack(fill=BOTH, side=TOP, expand=True)
+        button5.pack(fill=BOTH, side=TOP, expand=True)
+        button6.pack(fill=BOTH, side=TOP, expand=True)
+        # button1.grid(row=1, column=1, padx=50, pady=50)
+        # button2.grid(row=1, column=2, padx=50, pady=50)
+        # button3.grid(row=2, column=1, padx=50, pady=50)
+        # button4.grid(row=2, column=2, padx=50, pady=50)
 root = Tk()
 # root.configure(background="light blue")
 root.title("Random Generate")
-root.geometry("450x250")
+root.geometry("300x280")
 
 app = Window(root)
 root.mainloop()
